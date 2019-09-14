be2e3a9d162fd87ccfe226dde893a35ea841790c
Eric Harding <eric.harding@bluetradesys.com>
2019-09-14 15:53:23 -0400

    build front end on container start

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -3,5 +3,6 @@ rm /clinwiki/tmp/pids/server.pid
 bundle install
 bundle exec rake db:create
 bundle exec rake db:migrate
+(cd front; CW_PATH=.. scripts/build)
 bundle exec sidekiq -C config/sidekiq.yml &
 bundle exec rails server -b 0.0.0.0
