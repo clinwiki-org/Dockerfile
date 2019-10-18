e6a48f0d66f10c3c17a2e68859db828bd24b4277
Eric Harding <eric.harding@bluetradesys.com>
2019-10-17 20:47:42 -0400

    Add readme indicating docker-compose.yml has moved

--- compose/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm /clinwiki/tmp/pids/server.pid
-bundle install
-bundle exec rake db:create
-bundle exec rake db:migrate
-(cd front; CW_PATH=.. scripts/build)
-bundle exec sidekiq -C config/sidekiq.yml &
-bundle exec rails server -b 0.0.0.0
---
+++ compose/readme.md
@@ -0,0 +1,5 @@
+# Contents moved
+Clinwiki compose configuration is now part of the main repo here:  
+
+[https://github.com/clinwiki-org/clinwiki/tree/master/compose](https://github.com/clinwiki-org/clinwiki/tree/master/compose)
+
